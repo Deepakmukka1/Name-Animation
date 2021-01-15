f01020a09d7aff53d8ab72f81f32e22be28b7ae7
Mukka Deepak <ramu92013@gmail.com>
2021-01-15 22:20:39 +0530

    Updated App.js

--- app.js
+++ app.js
@@ -1,3 +1,4 @@
+//This file is just to know the exact lengths of each path
 const logo=document.querySelectorAll('svg path');
 for(let i=0;i<logo.length;i++)
 console.log(`ele ${i} : ${logo[i].getTotalLength()}`)
